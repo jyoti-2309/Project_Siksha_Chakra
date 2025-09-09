58523624fc8c1cca9d48090f00242f7782fa298f
Jyotiranjan <jyotiranjan1324@email.com>
2025-09-10 00:05:48 +0530

    third commit to siksha-chakra

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors())
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 const db = require('./db');
